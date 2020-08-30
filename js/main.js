be8b925ee574d7b236bcc864ef42e5c53f7f1918
Katerina Sidorenko <sidorennko21@gmail.com>
2020-08-30 16:49:30 +0300

    init project

---
+++ js/main.js
@@ -0,0 +1,25 @@
+
+$('.first-slider').slick({
+	infinite: true, // цикличная карусель
+	slidesToShow: 1,
+	slidesToScroll: 1,
+	// arrows: false, // скрыть кнопки переключения (вообще)
+	nextArrow: $('.button-next'),
+	prevArrow: $('.button-prev'),
+});
+
+$('.clients-slider').slick({
+	infinite: true,
+	slidesToShow: 1,
+	slidesToScroll: 1,
+	nextArrow: $('.button-forward-clients'),
+	prevArrow: $('.button-back-clients'),
+});
+
+$('.reviews-slider').slick({
+	infinite: true,
+	slidesToShow: 1,
+	slidesToScroll: 1,
+	nextArrow: $('.button-next-reviews'),
+	prevArrow: $('.button-prev-reviews'),
+});
